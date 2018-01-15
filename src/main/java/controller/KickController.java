6d684428ddfafaef0c95d2689244f955014a3135
madojskii <19madoy94@gmail.com>
2018-01-15 22:28:09 +0100

    kickowanie

---
+++ src/main/java/controller/KickController.java
@@ -0,0 +1,100 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.datastore.DatastoreFailureException;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+
+import model.Event;
+import model.User;
+
+import com.google.appengine.api.datastore.Key;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "kick", value = "/kick")
+public class KickController extends HttpServlet {
+
+	DatastoreService datastore;
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String paramUserName = "userid";
+		String paramUserNameValue = req.getParameter(paramUserName);
+		String paramEventId = "eventid";
+		String paramEventIdValue = req.getParameter(paramEventId);
+		System.out.println(paramUserNameValue);
+		Key key = KeyFactory.createKey("Event", Long.parseLong(paramEventIdValue));
+		Key keyUser = KeyFactory.createKey("User", paramUserNameValue);
+		Entity e1 = null;
+		Event event = null;
+		User user = null;
+		Entity postUser = null;
+		try {
+			e1 = datastore.get(key);
+			event = new Event(e1.getProperty("name").toString(), e1.getProperty("data").toString(),
+					e1.getProperty("description").toString());
+			postUser = datastore.get(keyUser);
+			user = new User(postUser.getKey().getName());
+			ArrayList<String> users = (ArrayList<String>)e1.getProperty("list");
+			event.setUsers(users);
+			System.out.println(users+" +");
+			int i =0;
+			for (Iterator<String> iterator = users.iterator(); iterator.hasNext();) {
+				String value = iterator.next();
+		        if (value.equalsIgnoreCase(user.getEmail())) {
+		            users.remove(i);
+		            break;
+		        }
+		        i++;
+		    }
+			event.setUsers(users);
+			user.setEvent(null);
+			//postUser.setProperty("email", user.getEmail());
+			postUser.setProperty("Event", null);
+		} catch (EntityNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			datastore.put(e1); // store the entity
+			datastore.put(postUser);
+			req.getRequestDispatcher("/index.jsp").forward(req, resp);
+		} catch (DatastoreFailureException e) {
+			throw new ServletException("Datastore error", e);
+		}
+
+	}
+
+	@Override
+	public void init() throws ServletException {
+
+		// setup datastore service
+		datastore = DatastoreServiceFactory.getDatastoreService();
+	}
+}
