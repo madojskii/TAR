5d62ac785afd5c06dfcb2f7f663f1b064701886a
madojskii <19madoy94@gmail.com>
2018-01-16 01:52:26 +0100

    mail manual(poprawki)

--- src/main/java/service/MailService.java
+++ src/main/java/service/MailService.java
@@ -8,6 +8,7 @@ import java.util.Locale;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
@@ -77,14 +78,62 @@ public class MailService extends HttpServlet {
 		}
 		resp.getWriter().print("Sending simple email.");
 	}
+	
+/*	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
+		Query q = new Query("Event");
+		PreparedQuery pq = datastore.prepare(q);
+		List<Event> lista = new ArrayList();
+		for (Entity e : pq.asIterable()) {
+			// System.out.println(e.getKey().getId());
+			Event event = new Event(e.getKey().getId(), e.getProperty("name").toString(),
+					e.getProperty("data").toString(), e.getProperty("description").toString());
+			event.setUsers((ArrayList<String>) e.getProperty("list"));
+			lista.add(event);
+		}
+		Calendar cal = Calendar.getInstance();
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		cal.add(Calendar.DATE, +1);
+		List<String> users = new ArrayList<String>();
+		for (Event e : lista) {
+			String string = e.getData();
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+			LocalDate date = LocalDate.parse(string, formatter);
+			System.out.println(date);
+			if (date.toString().equalsIgnoreCase(dateFormat.format(cal.getTime()).toString())) {
+				System.out.println("tak");
+				if (e.getUsers() != null && e.getUsers().size() > 1) {
+					users.addAll(e.getUsers());
+					System.out.println(e.getUsers().toString() + " +");
+				}
+			}
+		}
+		if (users.size() != 0) {
+			System.out.println("tutaj");
+			sendSimpleMail(users);
+		}
+		resp.getWriter().print("Sending simple email.");
+	}
+*/
 	private void sendSimpleMail(List<String> users) {
 		Properties props = new Properties();
-		Session session = Session.getDefaultInstance(props, null);
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+		Session session = Session.getDefaultInstance(props,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication("emaildozaliczeniatar@gmail.com","qazxsw12");
+					}
+				});
 
 		try {
 			Message msg = new MimeMessage(session);
-			msg.setFrom(new InternetAddress("19madoy94@gmail.com", "Admin"));
+			msg.setFrom(new InternetAddress("emaildozaliczeniatar@gmail.com", "Admin"));
 			// msg.addRecipient(Message.RecipientType.TO, new
 			// InternetAddress("marcin.miniuk@gmail.com", "Mr. User"));
 			for (String odb : users) {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,21 +1,17 @@
 <%@include file="/layout.jsp"%>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
-  <head>
-    <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
-    <title>Hello App Engine</title>
-  </head>
-
-  <body>
-    <h1>Hello App Engine!</h1>
-  
-    <table>
-      <tr>
-        <td colspan="2" style="font-weight:bold;">Available Servlets:</td>        
-      </tr>
-      <tr>
-        <td><a href='/hello'>The servlet</a></td>
-      </tr>
-    </table>
-  </body>
+<%@ page language="java" pageEncoding="utf-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Registration Success</title>
+</head>
+<body>
+	<div class="container" align="center">
+		<table class="table table-bordered">
+			<h2>Dzień dobry, witaj.</h2>
+		</table>
+	</div>
+</body>
 </html>
